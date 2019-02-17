0ee055998693c6f3b25582e98ec7b98e1550be68
xhrist14n <xhrist14n@gmail.com>
2019-02-17 02:45:41 -0500

    install

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
+apk add nano 
+nano /etc/apk/repositories
+apk update
 setup-xorg-base
-apk add xfce4 lxdm
+apk add alpine-desktop
+apk add xfce4 
+apk add thunar-volman
+apk add faenza-icon-theme
+apk add slim
+apk add lxdm
+rc-service dbus start
+rc-update add dbus
+rc-service udev start
+rc-update add udev
+rc-update add slim
 apk add xf86-video-qxl
 apk add xf86-input-keyboard xf86-input-mouse xf86-input-evdev
 apk add tango-icon-theme
 echo "http://nl.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
 apk update
 apk add virtualbox-additions-grsec
-apk add 
+apk add kbd 
